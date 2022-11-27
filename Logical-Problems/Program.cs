965407bbf69e789d4716d16022c03320b20bce6f
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-27 21:36:17 +0530

    Logical Program

---
+++ Logical-Problems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Logical_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Program");
+        }
+    }
+}
